c42e218fc92045b4cf8f9f7855997b16b803b44d
jaym2109 <jason@ontariopanelization.com>
2019-01-23 08:35:52 -0500

    made application production ready

---
+++ public/precache-manifest.243936140efad5eb4fdbf7ca2caf0af5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15ef94746031f8853af907909881f2a",
+    "url": "/static/media/logo.b15ef947.png"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/static/media/loading.584b607f.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2421c8a857c1a9fc1953",
+    "url": "/static/js/main.2421c8a8.chunk.js"
+  },
+  {
+    "revision": "debeacc3e49a6e943c32",
+    "url": "/static/js/1.debeacc3.chunk.js"
+  },
+  {
+    "revision": "2421c8a857c1a9fc1953",
+    "url": "/static/css/main.9cc6f319.chunk.css"
+  },
+  {
+    "revision": "a6f7e7fe0a69de93a4bc64ed45f34870",
+    "url": "/index.html"
+  }
+];
